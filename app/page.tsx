fd820da9f5c6c8d5260bc36b3c7fa668084917a2
anu-pama-pr <anupamap603@gmail.com>
2025-09-02 09:30:12 +0530

    issues fixed with hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,11 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Header from "./_components/Header";
 import { PopularCityList } from "./_components/PopularCityList";
-import HeroSection from "./_components/Hero.";
+import HeroSection from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      {/* <Header /> */}
       <HeroSection /> {/* Hero Section (name Hro changed to HeroSection) */}
       <PopularCityList />
     </div>
